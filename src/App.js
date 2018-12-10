4b606b386dbd75dc3d5d2dfccaf2de65fc4738e2
alexeytas <alexdali@gmail.com>
2018-12-11 02:17:40 +0600

    added new functions for ItemPage, CartPage, Item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Nav from './Nav';
 import ItemPage from './ItemPage';
 import CartPage from './CartPage';
@@ -26,6 +27,46 @@ class App extends Component {
     });
   }
 
+  handleRemoveOne = (item) => {
+    const {cart} = this.state;
+    //let newCart = cart.filter (el => item.id !== el.id);
+    let index = cart.indexOf(item.id);
+    this.setState({
+      cart: [ ...cart.slice(0,index), ...cart.slice(index + 1) ]
+    });
+  }
+
+  renderCart() {
+    // Count how many of each item is in the cart
+    let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0;
+      itemCounts[itemId]++;
+      return itemCounts;
+    }, {});
+
+    // Create an array of items
+    let cartItems = Object.keys(itemCounts).map(itemId => {
+      // Find the item by its id
+      var item = items.find(item =>
+          item.id === parseInt(itemId, 10)
+      );
+
+      // Create a new "item" and add the 'count' property
+      return {
+        ...item,
+        count: itemCounts[itemId]
+      }
+    });
+
+    return (
+      <CartPage
+        items={cartItems}
+        onAddOne={this.handleAddToCart}
+        onRemoveOne={this.handleRemoveOne}
+      />
+    );
+  }
+
   renderContent() {
     switch(this.state.activeTab) {
       default:
@@ -33,14 +74,12 @@ class App extends Component {
                         items={items}
                         onAddToCart={this.handleAddToCart}
                       />
-      case 1: return  <CartPage
-                        items={this.state.cart}
-                      />
+      case 1: return  this.renderCart();
     }
   }
 
   render() {
-    let {activeTab, cart } = this.state;
+    let {activeTab } = this.state;
 
     return (
       <div className="App">
