37f9c6a49b96192d3c5ad838fd03b711665791cf
alexeytas <alexdali@gmail.com>
2018-12-11 20:29:54 +0600

    added new TotalPrice

--- src/App.js
+++ src/App.js
@@ -20,23 +20,73 @@ class App extends Component {
     });
   }
 
+  // handleAddToCart = (item) => {
+  //   const {cart} = this.state;
+  //   this.setState({
+  //     cart: [ ...cart, item.id ]
+  //   });
+  // }
   handleAddToCart = (item) => {
     const {cart} = this.state;
-    this.setState({
-      cart: [ ...cart, item.id ]
+    let indexItem =  cart.findIndex(el => item.id === el.id);
+    //console.log('indexItem ', indexItem);
+    this.setState(     
+        { 
+          cart:  indexItem != -1 ? 
+                  cart.map((el,index) => {
+                      return (index === indexItem ? 
+                        { ...el, count: ++el.count, totalPrice: el.count * el.price } 
+                      : el);
+                  })
+                : [ ...cart, { ...item, count: 1, totalPrice: item.price } ]        
     });
   }
 
+  // handleRemoveOne = (item) => {
+  //   const {cart} = this.state;
+  //   //let newCart = cart.filter (el => item.id !== el.id);
+  //   let index = cart.indexOf(item.id);
+  //   this.setState({
+  //     cart: [ ...cart.slice(0,index), ...cart.slice(index + 1) ]
+  //   });
+  // }
   handleRemoveOne = (item) => {
     const {cart} = this.state;
-    //let newCart = cart.filter (el => item.id !== el.id);
-    let index = cart.indexOf(item.id);
+    let indexItem =  cart.findIndex(el => item.id === el.id);
+    let countItem = cart[indexItem].count;
+    //console.log(`rem cart[${indexItem}]: ${cart[indexItem]}`);
+    //console.log('cart[indexItem].count - ', cart[indexItem].count );
     this.setState({
-      cart: [ ...cart.slice(0,index), ...cart.slice(index + 1) ]
+       cart: countItem>1 ? 
+                cart.map((el,index) => {
+                            return (index === indexItem ? 
+                              { ...el, count: el.count-1 } 
+                            : el);
+                        })
+                : cart.filter((el,index) => index !== indexItem )
     });
   }
 
-  renderCart() {
+  // handleRemoveItem = (item) => {
+  //   const {cart} = this.state;
+  //   this.setState({
+  //     cart: cart.filter(el => item.id !== el.id)
+  //   });
+  // }
+
+  // SummPriceCart = () => {
+  //   const {cart} = this.state;
+  //   // let totalPrice = cart.reduce(totalPrice,item => {
+  //   //     return totalPrice + item.count * item.price;
+  //   //         });
+  //   let totalCountCart = cart.reduce((totalCountCart,item) => {
+  //       totalCountCart.count = totalCountCart.count++ || 0;
+  //       totalCountCart.price += item.price;
+  //       return totalCountCart;
+  //   }, {});
+  // } 
+
+  _renderCart() {
     // Count how many of each item is in the cart
     let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
       itemCounts[itemId] = itemCounts[itemId] || 0;
@@ -79,14 +129,35 @@ class App extends Component {
     );
   }
 
+  // renderContent() {
+  //   switch(this.state.activeTab) {
+  //     default:
+  //     case 0: return <ItemPage
+  //                       items={items}
+  //                       onAddToCart={this.handleAddToCart}
+  //                     />
+  //     case 1: return  this.renderCart();
+  //   }
+  // }
   renderContent() {
+    const {cart} = this.state;
+    let totalPriceCart = cart.length>0 ? 
+        cart.reduce((totalPriceCart, item) => {
+              return totalPriceCart + item.totalPrice;
+            })
+        : 0;
     switch(this.state.activeTab) {
       default:
       case 0: return <ItemPage
                         items={items}
                         onAddToCart={this.handleAddToCart}
                       />
-      case 1: return  this.renderCart();
+      case 1: return  <CartPage
+                        items={cart}
+                        onAddOne={this.handleAddToCart}
+                        onRemoveOne={this.handleRemoveOne}
+                        totalPriceCart={totalPriceCart}
+                      />
     }
   }
 
